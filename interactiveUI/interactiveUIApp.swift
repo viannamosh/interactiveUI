aa40b2d3ddaadefcd22ed5a87cc4463503938a0c
Vianna Moshtaghi <vianna.mosh@gmail.com>
2025-08-06 14:52:44 -0700

    Initial Commit

---
+++ interactiveUI/interactiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactiveUIApp.swift
+//  interactiveUI
+//
+//  Created by Scholar on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct interactiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
